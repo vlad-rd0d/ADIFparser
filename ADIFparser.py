83e3fbe17e7e49998719f6a72eefd011a833d5f0
vlad-rd0d <56669940+vlad-rd0d@users.noreply.github.com>
2020-12-18 21:44:25 +1000

    Update ADIFparser.py

--- ADIFparser.py
+++ ADIFparser.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #-------------------------------------------------------------------------------
-# Name:        <ADIFparser>
-# Purpose:     <>
+# Name:        ADIFparser
+# Purpose:     
 #
 # Author:      Vladimir Shchekunov RD0D
 #
-# Created:     <>
+# Created:     
 # Copyright:
-# Licence:     <>
+# Licence:     GPL
 #-------------------------------------------------------------------------------
 
 adif = '<QSO_DATE:8>20171126<TIME_ON:4>0416<TIME_OFF:4>0416<CALL:6>JH1QDB<MODE:2>CW<FREQ:6>14.061<BAND:3>20M<RST_SENT:3>599<RST_RCVD:3>599<NAME:8>Kunihiko<QSL_SENT:1>N<QSL_RCVD:1>N<GRIDSQUARE:4>PM94<MY_GRIDSQUARE:6>PN68lt<TX_PWR:3>100<APP_CQRLOG_DXCC:2>JA<DXCC:3>339<ITUZ:2>45<CQZ:2>25<LOTW_QSL_SENT:1>Y<LOTW_QSLSDATE:8>20200702<LOTW_QSL_RCVD:1>Y<LOTW_QSLRDATE:8>20190116<EQS_QSL_SENT:1>Y<EQSL_QSLSDATE:8>20190116<EOR>'
